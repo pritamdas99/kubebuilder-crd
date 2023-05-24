a3013b511365ff4fa154d692bd0ad16e644228c1
Pritam <pritam@appscode.com>
2023-05-24 16:05:57 +0600

    first commit

---
+++ api/v1alpha1/kluster_types.go
@@ -0,0 +1,81 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+type ContainerSpec struct {
+	Image string `json:"image,omitempty"`
+	Port  int32  `json:"port,omitempty"`
+}
+
+type ServiceSpec struct {
+	//+optional
+	ServiceName string `json:"serviceName,omitempty"`
+	ServiceType string `json:"serviceType"`
+	//+optional
+	ServiceNodePort int32 `json:"serviceNodePort,omitempty"`
+	ServicePort     int32 `json:"servicePort"`
+}
+
+// KlusterSpec defines the desired state of Kluster
+type KlusterSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	Name      string        `json:"name,omitempty"`
+	Replicas  *int32        `json:"replicas"`
+	Container ContainerSpec `json:"container"`
+	Service   ServiceSpec   `json:"service,omitempty"`
+}
+
+// KlusterStatus defines the observed state of Kluster
+type KlusterStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	AvilableReplicas int32 `json:"availableReplicas"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Kluster is the Schema for the klusters API
+type Kluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   KlusterSpec   `json:"spec,omitempty"`
+	Status KlusterStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// KlusterList contains a list of Kluster
+type KlusterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Kluster `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Kluster{}, &KlusterList{})
+}
